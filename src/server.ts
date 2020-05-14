d0403a7d91ebe8ec7dbbbcdeb72df64f779b2b5a
Douglas Porto <douglasalexandre7@gmail.com>
2020-05-13 21:44:28 -0300

    Install prettier and debuger

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, resp) => resp.json('Hello World'));
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333');
