b57b2fc89c02de629bbe02535ad9f7a67122f617
Douglas Porto <douglasalexandre7@gmail.com>
2020-06-02 21:42:39 -0300

    middleware authenticate

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'abd2f885067fd34785f1991235894b15',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -26,9 +27,10 @@ class AuthenticateUserService {
     if (!passwordMatched)
       throw new Error('Incorrect email/password comnination');
 
-    const token = sign({}, 'abd2f885067fd34785f1991235894b15', {
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return { user, token };
