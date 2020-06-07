fc1a7534e1b0f22b916481f632fc0e5f60d1f2c8
Douglas Porto <douglasalexandre7@gmail.com>
2020-06-07 17:21:49 -0300

    upload by avatar

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,8 +1,13 @@
-import { Router } from 'express';
+import { Router, request } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, resp) => {
   try {
@@ -15,5 +20,25 @@ usersRouter.post('/', async (req, resp) => {
     return resp.status(400).json({ error: e.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, resp) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: req.user.id,
+        avatarFilename: req.file.filename,
+      });
+      delete user.password;
+      return resp.json(user);
+    } catch (e) {
+      return resp.status(400).json({ error: e.message });
+    }
+  },
+);
 
 export default usersRouter;
